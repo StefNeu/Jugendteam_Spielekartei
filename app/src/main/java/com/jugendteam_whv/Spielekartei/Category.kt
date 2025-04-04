79f8eb799336ca32d1857518b1bb364d466d8092
Steffen Neumann <neumann.steffen@gmx.net>
2025-04-04 11:32:27 +0200

    Games Details View
    Added:
    - Toolbar with game name
    - Category
    - Group Size
    - Age
    - Material
    - Description

--- app/src/main/java/com/jugendteam_whv/Spielekartei/Category.kt
+++ app/src/main/java/com/jugendteam_whv/Spielekartei/Category.kt
@@ -1,5 +1,20 @@
 package com.jugendteam_whv.Spielekartei
 
 enum class Category {
-    GROUP_ALLOCATION, GET_TO_KNOW, CIRCLE_GAME, SINGING_GAME, MOVEMENT_GAME, TERRAIN_GAME, TRUST_GAME, PRAYERS, IMPETUS, PUZZLE, DEFAULT
+    GROUP_ALLOCATION, GET_TO_KNOW, CIRCLE_GAME, SINGING_GAME, MOVEMENT_GAME, TERRAIN_GAME, TRUST_GAME, PRAYERS, IMPETUS, PUZZLE, DEFAULT;
+    override fun toString(): String {
+        return when (this) {
+            GROUP_ALLOCATION -> "Gruppeneinteilung"
+            GET_TO_KNOW -> "Kennenlernen"
+            CIRCLE_GAME -> "Kreisspiel"
+            SINGING_GAME -> "Singespiel"
+            MOVEMENT_GAME -> "Bewegungsspiel"
+            TERRAIN_GAME -> "Geländespiel"
+            TRUST_GAME -> "Vertrauensspiel"
+            PRAYERS -> "Gebete"
+            IMPETUS -> "Impuls"
+            PUZZLE -> "Rätsel"
+            DEFAULT -> "Standard"
+        }
+    }
 }
--- app/src/main/java/com/jugendteam_whv/Spielekartei/games_details.kt
+++ app/src/main/java/com/jugendteam_whv/Spielekartei/games_details.kt
@@ -2,12 +2,21 @@ package com.jugendteam_whv.Spielekartei
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class games_details : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var categoryFild: TextView
+    private lateinit var sizeFild: TextView
+    private lateinit var ageFild: TextView
+    private lateinit var materialFild: TextView
+    private lateinit var descriptionFild: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +29,18 @@ class games_details : AppCompatActivity() {
         var gameStore: GameStore = GameStore.getInstance()
         var game: Game = gameStore.selectedGame!!
         Log.d("Games Details",game.name )
+        toolbar = findViewById(R.id.toolbar)
+        categoryFild = findViewById(R.id.category)
+        sizeFild = findViewById(R.id.size)
+        ageFild = findViewById(R.id.age)
+        materialFild = findViewById(R.id.material)
+        descriptionFild = findViewById(R.id.description)
+        toolbar.setTitle(game.name)
+        categoryFild.setText(game.category.toString())
+        var groupSizeString: String = game.groupSizeMin.toString() + " - " + game.groupSizeMax.toString()
+        sizeFild.setText(groupSizeString)
+        ageFild.setText(game.age.toString())
+        materialFild.setText(game.material)
+        descriptionFild.setText(game.description)
     }
 }
